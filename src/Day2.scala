925ee8f4aa0532472250bf6223bf63df2d811765
kaspar <kaspar@hellden.com>
2021-12-02 09:47:19 +0100

    learning scala 3 enum

--- src/Day2.scala
+++ src/Day2.scala
@@ -1,19 +1,41 @@
 object Day2 extends Base(2):
 
-  private val Pattern = """(\w+) (\d+)""".r
+  enum Command:
+    case Up(units: Int)
+    case Down(units: Int)
+    case Forward(units: Int)
+
+  object Command:
+
+    private val Pattern = """(\w+) (\d+)""".r
+
+    def parse(s: String): Command =
+      s match
+        case Pattern(name, units) =>
+          apply(name, units.toInt)
+
+    private def apply(name: String, units: Int): Command =
+      name match
+        case "up" => Up(units)
+        case "down" => Down(units)
+        case "forward" => Forward(units)
+
+  import Command._
+
+  private val commands = inputLines.map(parse)
 
   override val part1: Int = // 1924923
-    val (position, depth) = inputLines.foldLeft((0, 0)) {
-      case ((x, y), Pattern("forward", steps)) => (x + steps.toInt, y)
-      case ((x, y), Pattern("up", steps)) => (x, y - steps.toInt)
-      case ((x, y), Pattern("down", steps)) => (x, y + steps.toInt)
+    val (position, depth) = commands.foldLeft((0, 0)) {
+      case ((x, y), Up(units)) => (x, y - units)
+      case ((x, y), Down(units)) => (x, y + units)
+      case ((x, y), Forward(units)) => (x + units, y)
     }
     position * depth
 
   override val part2: Int = // 1982495697
-    val (position, depth, _) = inputLines.foldLeft((0, 0, 0)) {
-      case ((x, y, aim), Pattern("forward", steps)) => (x + steps.toInt, y + aim * steps.toInt, aim)
-      case ((x, y, aim), Pattern("up", steps)) => (x, y, aim - steps.toInt)
-      case ((x, y, aim), Pattern("down", steps)) => (x, y, aim + steps.toInt)
+    val (position, depth, _) = commands.foldLeft((0, 0, 0)) {
+      case ((x, y, aim), Up(units)) => (x, y, aim - units)
+      case ((x, y, aim), Down(units)) => (x, y, aim + units)
+      case ((x, y, aim), Forward(units)) => (x + units, y + aim * units, aim)
     }
     position * depth
